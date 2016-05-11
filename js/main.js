28eb9a7fb5e8d15df540754464cdbe329825c2c6
Matthew Hendricks <mscohendricks@gmail.com>
2016-05-10 22:34:20 -0600

    lines are working (finally)

--- js/main.js
+++ js/main.js
@@ -1,89 +1,5 @@
 (function() {
-    var Line, borderWidth, connect, windowHeight, windowWidth;
-    var currentLine = 1;
-    connect = function(element) {
-        var $stepLine, line, stepLine;
-        if (element.hasClass('connected')){
-            return;
-        }
-        if (!element.is(':first-child') && !element.prev().hasClass('connected')) {
-            return;
-        }
-        line = new Line(element, element.next());
-     
-        
 
-        stepLine = '<div  class="animated-line-container" id="line_' + line.id + '"/>';
-
-      
-        //   stepLine = '<div class="animated-line-container" id="' + line.id + '" style=\'display: none; position: absolute; transform: rotate( ' + line.angle + 'deg); height: 0px; left: ' + line.left + 'px; top:' + line.top + 'px; width: 0px; right: ' + line.right + 'px; animation-direction: ' + line.direction + '\'/>';
-        $stepLine = $(stepLine);
-        $('#svg_canvas').append($stepLine);
-        $stepLine.fadeIn(20000);
-        element.addClass('connected');
-    
-    };
-    Line = function(activeElement, nextElement) {
-
-        this.top = 5;
-        this.right = 5;
-        this.left = 5;
-        this.width = 5;
-        this.height = 5;
-        // var activeCenter, activeLeft, activeTop, activeHeight, activeWidth, nextCenter, nextLeft, nextTop, tmpRight, tmpTop, topDifference;
-        // this.id = activeElement.attr('id');
-       
-        // if(nextElement.length < 1){
-        //     nextElement = activeElement;
-        // }
-        
-        // activeLeft = activeElement.offset().left;
-        // activeWidth = activeElement.width();
-        // activeHeight = activeElement.height();
-        // activeTop = activeElement.offset().top;
-        // nextTop = nextElement.offset().top;
-        // nextLeft = nextElement.offset().left;
-        // activeCenter = activeLeft + activeWidth / 2;
-        // nextCenter = nextLeft - activeLeft;
-        // topDifference = activeTop - nextTop;
-        // tmpRight = 0;
-        // if (activeTop < nextTop) {
-        //     tmpTop = activeTop - topDifference;
-        //     this.direction = 'reverse';
-        //     this.angle = 180;
-        // } else {
-        //     tmpTop = activeTop - topDifference;
-        //     this.direction = 'forwards';
-        // }
-        // if (activeCenter > windowWidth * 0.8 && activeTop < windowHeight * 0.5) {
-        //     nextCenter = windowWidth - activeCenter - borderWidth * 4;
-        //     topDifference = nextTop - activeTop;
-        // }
-        // if (activeLeft > nextLeft && activeTop > activeHeight) {
-        //     nextCenter = activeLeft - nextLeft;
-        //     activeCenter = nextLeft + nextElement.width() / 2;
-        // }
-        // if (activeTop > windowHeight * 0.75) {
-        //     tmpTop = activeTop -= topDifference + borderWidth * 2;
-        //     nextCenter = activeLeft - nextLeft - borderWidth * 2;
-        //     tmpRight = windowWidth - activeCenter - nextCenter - borderWidth * 2;
-        //     activeCenter = null;
-        // }
-        // if (topDifference < 0) {
-        //     topDifference = Math.abs(topDifference);
-        // }
-        // if (nextCenter < 5) {
-        //     tmpTop = activeTop - topDifference;
-        //     nextCenter = null;
-        //     tmpRight = windowWidth - activeTop
-        // }
-       
-        // this.top = tmpTop;
-        // this.right = tmpRight;
-        // this.left = activeCenter;
-        // this.width = nextCenter;
-        // this.height = topDifference;
-    };
     jQuery.fn.showHoverText = function() {
         var text;
         text = this.find('.text-container').animate({
@@ -108,10 +24,122 @@
     windowWidth = $(window).width();
     windowHeight = $(window).height();
     $('.brewing-step').hover(function() {
-        connect($(this));
+        // connect($(this));
     });
     $(window).resize(function() {
         $('.brewing-step').removeClass('connected');
         $('.animated-line-container').remove();
     });
 }.call(this));
+
+
+
+
+var Belay = (function(){
+  var settings = {
+                    strokeColor       : '#fff',
+                    strokeWidth       : 2,
+                    opacity           : 1,
+                    fill              : 'none',
+                    animate           : true,
+                    animationDirection: 'right',
+                    animationDuration : .75
+                 };
+  var me = {};
+
+  me.init = function(initObj) {
+      if (initObj) {
+        $.each(initObj, function(index, value) {
+           //TODO validation on settings
+           settings[index] = value;
+        });
+      }
+  }
+
+  me.set = function(prop, val){
+    //TODO validate
+    settings[prop] = val;
+  }
+
+  me.on = function(el1, el2){
+    var $el1 = $(el1);
+    var $el2 = $(el2);
+    if ($el1.length && $el2.length) {
+      var svgheight
+          ,p
+          ,svgleft
+          ,svgtop
+          ,svgwidth
+
+      var el1pos = $(el1).offset();
+      var el2pos = $(el2).offset();
+
+      var el1H = $(el1).outerHeight();
+      var el1W = $(el1).outerWidth();
+
+      var el2H = $(el2).outerHeight();
+      var el2W = $(el2).outerWidth();
+
+      svgleft = Math.round(el1pos.left + el1W);
+      svgwidth = Math.round(el2pos.left - svgleft);
+
+      var curvinessFactor, cpt;
+
+      ////Determine which is higher/lower
+      if( (el2pos.top+(el2H/2)) <= ( el1pos.top+(el1H/2))){
+        // console.log("low to high");        
+        svgheight = Math.round((el1pos.top+el1H/2) - (el2pos.top+el2H/2));
+        svgtop = Math.round(el2pos.top + el2H/2) - settings.strokeWidth;        
+        cpt = Math.round(svgwidth*Math.min(svgheight/300, 1));
+        p = "M0,"+ (svgheight+settings.strokeWidth) +" C"+cpt+","+(svgheight+settings.strokeWidth)+" "+(svgwidth-cpt)+"," + settings.strokeWidth + " "+svgwidth+"," + settings.strokeWidth;          
+      }else{
+        // console.log("high to low");
+        svgheight = Math.round((el2pos.top+el2H/2) - (el1pos.top+el1H/2));
+        svgtop = Math.round(el1pos.top + el1H/2) - settings.strokeWidth;  
+        cpt = Math.round(svgwidth*Math.min(svgheight/300, 1));
+        p = "M0," + settings.strokeWidth + " C"+ cpt +",0 "+ (svgwidth-cpt) +","+(svgheight+settings.strokeWidth)+" "+svgwidth+","+(svgheight+settings.strokeWidth);                  
+      }
+      
+      //ugly one-liner
+      $ropebag = $('#ropebag').length ? $('#ropebag') : $('body').append($( "<div id='ropebag' />" )).find('#ropebag');
+
+      var svgnode = document.createElementNS('http://www.w3.org/2000/svg','svg');
+      var newpath = document.createElementNS('http://www.w3.org/2000/svg',"path");
+      newpath.setAttributeNS(null, "d", p);
+      newpath.setAttributeNS(null, "stroke", settings.strokeColor);
+      newpath.setAttributeNS(null, "stroke-width", settings.strokeWidth);
+      newpath.setAttributeNS(null, "opacity", settings.opacity);
+      newpath.setAttributeNS(null, "fill", settings.fill);      
+      svgnode.appendChild(newpath);
+      //for some reason, adding a min-height to the svg div makes the lines appear more correctly.
+      $(svgnode).css({left: svgleft, top: svgtop, position: 'absolute',width: svgwidth, height: svgheight + settings.strokeWidth*2, minHeight: '20px' });
+      $ropebag.append(svgnode);
+      if (settings.animate) {
+        // THANKS to http://jakearchibald.com/2013/animated-line-drawing-svg/
+        var pl = newpath.getTotalLength();
+        // Set up the starting positions
+        newpath.style.strokeDasharray = pl + ' ' + pl;
+
+        if (settings.animationDirection == 'right') {
+          newpath.style.strokeDashoffset = pl;
+        } else {
+          newpath.style.strokeDashoffset = -pl;
+        }
+
+        // Trigger a layout so styles are calculated & the browser
+        // picks up the starting position before animating
+        // WON'T WORK IN IE. If you want that, use requestAnimationFrame to update instead of CSS animation
+        newpath.getBoundingClientRect();
+        newpath.style.transition = newpath.style.WebkitTransition ='stroke-dashoffset ' + settings.animationDuration + 's ease-in-out';
+        // Go!
+        newpath.style.strokeDashoffset = '0';
+      }
+    }
+  }
+
+  me.off = function(){
+    $("#ropebag").empty();
+  }
+
+  return me;
+}());
